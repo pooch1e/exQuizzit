ffc79842a5481e999e4a638007c84884430dd5ff
Joel Kram <joelnskram@gmail.com>
2025-07-07 16:23:14 +0100

    add: prisma/seed file

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+//seed file
+import prisma from "../src/app/lib/connections";
+import {users, countries, games} from '../src/data/test-data/index'
+
+async function main() {
+  
+  await prisma.game.deleteMany({})
+  await prisma.user.deleteMany({})
+  await prisma.countries.deleteMany({})
+
+  // Seed countries
+  await prisma.countries.createMany({
+    data: countries,
+    skipDuplicates: true 
+  })
+
+  // Seed users
+  await prisma.user.createMany({
+    data: users,
+    skipDuplicates: true
+  })
+
+  // Seed game attempts
+  await prisma.game.createMany({
+    data: games,
+    skipDuplicates: true
+  })
+}
+
+main()
+  .catch((error) => {
+    console.error('Seeding error:', error)
+    process.exit(1)
+  })
+  .finally(() => prisma.$disconnect())
