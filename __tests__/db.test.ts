286ccda823b5589200b00209b35a182f63c9a0b2
Bradley Mattison <bradleymattison2000@gmail.com>
2025-07-08 11:18:02 +0100

    add tests to check data has seeded into database tables - tests will be developed further in next commit

--- __tests__/db.test.ts
+++ __tests__/db.test.ts
@@ -1,6 +1,7 @@
-import {prisma} from "../src/app/lib/prisma.ts"
+import { prisma } from "../src/app/lib/prisma.ts"
 import { seedTestDatabase } from "../prisma/seed-test-db.ts"
 import { cleanTestDatabase, resetTestSequences } from "../src/app/lib/prisma.ts";
+
   beforeEach(async () => {
     // Clean and re-seed before each test in this suite
     await cleanTestDatabase();
@@ -15,10 +16,24 @@ import { cleanTestDatabase, resetTestSequences } from "../src/app/lib/prisma.ts"
 describe('testing test-seed database', () => {
 
 
-  test('should find seeded countries', async () => {
+  test('should find seeded countries table', async () => {
     // console.log(process.env) //showing test env
     const countries = await prisma.countries.findMany();
     expect(countries.length).toBeGreaterThan(0);
-    console.log(countries) // ensuring test is working
+     
+  });
+
+  test('should find seeded users table', async () => {
+    // console.log(process.env) //showing test env
+    const user = await prisma.user.findMany();
+    expect(user.length).toBeGreaterThan(0);
+    
+  });
+
+  test('should find seeded games table', async () => {
+    // console.log(process.env) //showing test env
+    const game = await prisma.game.findMany();
+    expect(game.length).toBeGreaterThan(0);
+    console.log(game) // ensuring test is working
   });
 })
