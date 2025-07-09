162f35e3fe53b4d307772100795c1dfdb58f2fb0
Joel Kram <joelnskram@gmail.com>
2025-07-09 10:53:42 +0100

    add: test for shuffle array util

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,10 +1,19 @@
-import { shuffleArray } from "../src/app/lib/"
+import { shuffleArray } from "../src/app/lib/utils/shuffleArray"
 describe('shuffling array util function', () => {
-  test('when passed empty array, returns emtpy array', () => {
+  test('when passed empty array, returns empty array', () => {
+    const emptyArray : [] = [];
+    const actual = shuffleArray(emptyArray)
+    expect(actual).toEqual([]);
+  })
+  test('returns array of same length', () => {
     const testData = ['Australia', 'Japan', 'Mexico']
     const actual = shuffleArray(testData)
     expect(actual).toHaveLength(3)
   })
-  test.todo('when passed array with single item, returns single item')
-  test.todo('when passed array of multiple items, returns array with items shuffled')
+  test('does not mutate original array', () => {
+    const testData = ['Australia', 'Japan', 'Mexico']
+    const copy = [...testData]
+    shuffleArray(testData)
+    expect(testData).toEqual(copy);
+  })
 })
