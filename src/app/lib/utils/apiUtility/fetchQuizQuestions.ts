2194456f7a7f6972f55799246b653e129403016c
Joel Kram <joelnskram@gmail.com>
2025-07-17 13:32:44 +0100

    fixing fetch questions function

--- src/app/lib/utils/apiUtility/fetchQuizQuestions.ts
+++ src/app/lib/utils/apiUtility/fetchQuizQuestions.ts
@@ -2,9 +2,11 @@ import { Question } from '@/types/Question.ts/Questions';
 
 export async function fetchQuizQuestions(): Promise<Question[] | null> {
   try {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+    // const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
-    const result = await fetch(`${baseUrl}/api/quiz/questions`);
+    // const result = await fetch(`${baseUrl}/api/quiz/questions`);
+    const result = await fetch('/api/quiz/questions');
+    
 
     if (!result.ok) {
       console.log('error fetching quiz questions');
@@ -12,6 +14,7 @@ export async function fetchQuizQuestions(): Promise<Question[] | null> {
     }
 
     const data = await result.json();
+    console.log('feteched questions correctly', data.questions)
 
     // Ensure each question has the required 'type' property
     if (
